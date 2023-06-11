ef70092a10f6dfb615fe3001f001ffee3e8771b4
techswarn <tech.swarns@gmail.com>
2023-06-11 10:45:37 +0530

    code update

--- scripts/checkEndpoint.js
+++ scripts/checkEndpoint.js
@@ -1,18 +1,24 @@
 const axios = require('axios')
 
 const checkEndpoint = async () => {
-    const {data} = await axios.post('https://integratestg.e-food.gr/api/v1/partners/login',{
-        "username":"captainspec",
-        "password":"captainspecX!foody2023"
-    }, {
-        headers: {
-            'Content-Type': 'application/json',
-            
-          }
-    })
+    let data;
+    try {
+        const res = await axios.post('https://integratestg.e-food.gr/api/v1/partners/login',{
+            "username":"captainspec",
+            "password":"captainspecX!foody2023"
+        }, {
+            headers: {
+                'Content-Type': 'application/json',
+                
+              }
+        })
+        data = res
+    }catch(err){
+        console.log(err)
+    }
 
     console.log(data)
-    return data
+    return data?.data
 }
 
 exports.checkEndpoint = checkEndpoint
