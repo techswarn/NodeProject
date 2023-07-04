01755ba5ff55c33daff9492259f22101cc30924f
techswarn <tech.swarns@gmail.com>
2023-07-04 14:14:53 +0530

    code update

--- scripts/dbmysqlcheck.js
+++ scripts/dbmysqlcheck.js
@@ -1,54 +1,54 @@
-const mysql = require('mysql');
-const dotenv = require('dotenv')
-const fs = require('fs')
+const mysql = require("mysql");
+const dotenv = require("dotenv");
+const fs = require("fs");
 
-dotenv.config({path: './config.env'})
+dotenv.config({ path: "./config.env" });
 
 const mysqlDbConnect = async () => {
+  let failure = null;
+  const sslFilePath = `${__dirname}/../assets/ca-certificate.crt`;
+  console.log(sslFilePath);
+  console.log(process.env.MYSQL_HOST);
+  console.log(process.env.MYSQL_PASSWORD);
+  console.log(process.env.MYSQL_USER);
+  console.log(process.env.MYSQL_DATABASE);
 
-    let failure = null;
-    const sslFilePath = `${__dirname}/../assets/ca-certificate.crt`
-    console.log(sslFilePath)
-    console.log(process.env.MYSQL_HOST)
-    console.log(process.env.MYSQL_PASSWORD)
-    console.log(process.env.MYSQL_USER)
-    console.log(process.env.MYSQL_DATABASE)
+  const connection = mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    port: process.env.MYSQL_PORT,
+    ssl: {
+      ca: fs.readFileSync(sslFilePath),
+    },
+  });
 
-    const connection = mysql.createConnection({
-        host     : process.env.MYSQL_HOST,
-        user     : process.env.MYSQL_USER,
-        password : process.env.MYSQL_PASSWORD,
-        database : process.env.MYSQL_DATABASE,
-        port     : process.env.MYSQL_PORT,
-        ssl  : {
-            ca : fs.readFileSync(sslFilePath)
-        }
-    });
+  await connection.connect((err) => {
+    if (err) {
+      console.log("Connection error: " + err);
+      failure = err;
+      return;
+    }
+  });
+  let row;
+  connection.query("select * from student", (err, result, fields) => {
+    if (err) {
+      console.log("err in query: " + err);
+    }
 
-    await connection.connect((err)=>{
-        if(err){
-            console.log("Connection error: " + err)
-            failure = err
-            return;
-        }
-    })
-    let row;
-    connection.query("select * from student", (err, result, fields) => {
-        if(err){
-            console.log('err in query: ' + err)
-        }
+    console.log(result);
+    row = result;
+  });
 
-        console.log(result)
-        row = result
-    });
-   
-    // try {
-    //     row = await connection.query( 'select * from student' );
-    // } catch ( err ) {
-    //     console.log('query error: ' + err)
-    // }
-    // console.log(row)
-    return row
-}
+  // try {
+  //     row = await connection.query( 'select * from student' );
+  // } catch ( err ) {
+  //     console.log('query error: ' + err)
+  // }
+  // console.log(row)
+  connection.end();
+  return row;
+};
 
-exports.mysqlDbConnect = mysqlDbConnect
+exports.mysqlDbConnect = mysqlDbConnect;
