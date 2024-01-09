6b4cc225159d3bc9330b0fcbe48059fd3878a046
techswarn <tech.swarns@gmail.com>
2024-01-09 18:01:26 +0530

    code update

--- scripts/dbpgpool.js
+++ scripts/dbpgpool.js
@@ -1,31 +1,30 @@
-const { Pool } = require('pg')
-const dotenv = require('dotenv')
+const { Pool } = require("pg");
+const dotenv = require("dotenv");
 
-dotenv.config({path: './config.env'})
+dotenv.config({ path: "./config.env" });
 
 const dbpoolcon = async () => {
+  console.log("Hostname: " + process.env.PG_HOST_POOL);
+  console.log("Database: " + process.env.PG_DATABASE_POOL);
+  console.log("Port: " + process.env.PG_PORT_POOL);
+  console.log("User: " + process.env.PG_USER_POOL);
+  console.log("Password: " + process.env.PG_PASSWORD_POOL);
 
-    console.log("Hostname: " + process.env.PG_HOST_POOL)
-    console.log("Database: " + process.env.PG_DATABASE_POOL)
-    console.log("Port: " + process.env.PG_PORT_POOL)
-    console.log("User: " + process.env.PG_USER_POOL)
-    console.log("Password: " + process.env.PG_PASSWORD_POOL)
+  const pool = new Pool({
+    user: process.env.PG_USER_POOL,
+    host: process.env.PG_HOST_POOL,
+    database: process.env.PG_DATABASE_POOL,
+    password: process.env.PG_PASSWORD_POOL,
+    port: process.env.PG_PORT_POOL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
 
-    const pool = new Pool({
-        user: process.env.PG_USER_POOL,
-        host: process.env.PG_HOST_POOL,
-        database: process.env.PG_DATABASE_POOL,
-        password: process.env.PG_PASSWORD_POOL,
-        port: process.env.PG_PORT_POOL,
-        ssl: {
-            rejectUnauthorized: false
-        }
-    })
+  const result = await pool.query("SELECT NOW()");
 
-    const result = await pool.query('SELECT * FROM company')
-    await pool.end()
-    return result   
-}
+  await pool.end();
+  return result;
+};
 
-exports.dbpoolcon = dbpoolcon
-
+exports.dbpoolcon = dbpoolcon;
